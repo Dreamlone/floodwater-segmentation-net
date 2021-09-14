251f9a3acef311e51924b64f83d6910d64f96cfe
ChrisLisbon <yulashka.htm@yandex.ru>
2021-09-14 15:58:13 +0300

    indices functions added

---
+++ preprocessing.py
@@ -0,0 +1,23 @@
+import numpy as np
+from scipy.ndimage import gaussian_filter
+from sklearn import preprocessing
+
+
+def rvi_index(vh_matrix, vv_matrix):
+    return 4*vh_matrix/(vh_matrix+vv_matrix)
+
+
+def swi_index(vh_matrix, vv_matrix):
+    return 0.1747*vv_matrix+0.0082*vh_matrix*vv_matrix+0.0023*np.power(vv_matrix, 2)-0.0015*np.power(vh_matrix, 2)+0.1904
+
+
+def ratio_index(vh_matrix, vv_matrix):
+    return vh_matrix/vv_matrix
+
+
+def gaussian_filter(matrix):
+    return gaussian_filter(matrix, sigma=5)
+
+
+def normalization(matrix):
+    return preprocessing.normalize(matrix, norm='l2')
