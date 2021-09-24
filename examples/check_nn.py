8900f26c64384e332b3cff80703d758d5f61e507
Dreamlone <mik_sar@mail.ru>
2021-09-24 17:23:29 +0300

    convert fix

--- examples/check_nn.py
+++ examples/check_nn.py
@@ -3,15 +3,16 @@ import torch.utils.data as data_utils
 
 from segtat.explorer import ModelExplorer
 
-explorer = ModelExplorer(working_dir='D:/segmentation', device='cuda')
+explorer = ModelExplorer(working_dir='D:/segmentation', device='cuda:0')
 # Load data as PyTorch tensors
-x_train, y_train = explorer.load_data(features_path='D:/segmentation/converted_sigma_2_vh_vv_swi/X_train.pt',
-                                      target_path='D:/segmentation/converted_sigma_2_vh_vv_swi/Y_train.pt',
+x_train, y_train = explorer.load_data(features_path='D:/segmentation/converted_final/X_train.pt',
+                                      target_path='D:/segmentation/converted_final/Y_train.pt',
                                       as_np=False)
+x_train = x_train.float()
 validation = data_utils.TensorDataset(x_train, y_train)
 # Validate model on the test dataset
 for th in [0.5]:
     print(f'Threshold: {th}')
     metrics = [smp.utils.metrics.IoU(threshold=th)]
-    explorer.validate(validation, model_path='D:/segmentation/fpn_01_00_22_09.pth',
+    explorer.validate(validation, model_path='D:/segmentation/best_model.pth',
                       metrics=metrics, threshold=th, vis=True)
